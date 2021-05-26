647043f011492beba874e20dae5e34b4bb8bc683
Victor Williams Stafusa da Silva <victorwssilva@gmail.com>
2021-05-26 10:25:22 -0300

    Improvements and fixes in the build process.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ import ninja.javahacker.reifiedgeneric.ReifiedGeneric;
 import ninja.javahacker.reifiedgeneric.Token;
 
 /**
- * Defines the {@link Token} and the {@link ReifiedGeneric} classes, which cretes a mechanism for declaring type tokens.
+ * Defines the {@link Token} and the {@link ReifiedGeneric} classes, which creates a mechanism for declaring type tokens.
  * <p>For example, you can't say {@code List<String>.class} due to type-erasure.
  * The solution is to use <code>Token&lt;String&gt; {}.getReified()</code> instead.</p>
  * @see ninja.javahacker.reifiedgeneric.ReifiedGeneric
